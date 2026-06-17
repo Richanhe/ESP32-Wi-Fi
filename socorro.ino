e66ad1964173f67d7a5fa88eac277134d4e2c7b0
Richard Oliveira <85001637+Richanhe@users.noreply.github.com>
2026-06-17 17:28:57 -0300

    Add HTTP server to control LED with endpoints

--- socorro.ino
+++ socorro.ino
@@ -1,10 +1,44 @@
 #include <WiFi.h>
+#include <WebServer.h>
 
 const char* ssid = "TP-LINK_1586";
 const char* password = "29270811";
 
+WebServer server(80);
+
+const int LED_PIN = 2;
+
+void ligarLed() {
+  digitalWrite(LED_PIN, HIGH);
+
+  server.send(200, "application/json",
+              "{\"status\":\"ligado\"}");
+}
+
+void desligarLed() {
+  digitalWrite(LED_PIN, LOW);
+
+  server.send(200, "application/json",
+              "{\"status\":\"desligado\"}");
+}
+
+void statusLed() {
+  bool estado = digitalRead(LED_PIN);
+
+  server.send(
+    200,
+    "application/json",
+    estado ?
+      "{\"led\":\"ligado\"}" :
+      "{\"led\":\"desligado\"}"
+  );
+}
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 
   Serial.println("Conectando ao WiFi...");
 
@@ -17,12 +51,24 @@ void setup() {
 
   Serial.println();
   Serial.println("Conectado!");
+
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 
-  Serial.print("MAC: ");
-  Serial.println(WiFi.macAddress());
+  // GET /ligar
+  server.on("/ligar", HTTP_GET, ligarLed);
+
+  // GET /desligar
+  server.on("/desligar", HTTP_GET, desligarLed);
+
+  // GET /status
+  server.on("/status", HTTP_GET, statusLed);
+
+  server.begin();
+
+  Serial.println("Servidor HTTP iniciado");
 }
 
 void loop() {
+  server.handleClient();
 }
